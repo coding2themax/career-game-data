4d1696c343f3a6d1d5300711d5428ba0405f637d
max quinones <maximilian.quinones@gmail.com>
2023-12-09 05:49:16 -0700

    init code structure

---
+++ src/main/java/com/coding2themax/career/game/careergamedata/CareerGameDataApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.game.careergamedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerGameDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerGameDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding2themax/career/game/careergamedata/CareerGameDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding2themax.career.game.careergamedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerGameDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
