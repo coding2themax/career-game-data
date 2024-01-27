75f6d780cf38e9493981d9561a834a9062079b32
max quinones <maximilian.quinones@gmail.com>
2024-01-27 09:10:41 -0700

    adding schema

--- src/main/resources/schema-all.sql
+++ src/main/resources/schema-all.sql
@@ -1,2 +1,2 @@
 CREATE SCHEMA ors;
-CREATE TABLE IF NOT EXIST ors.category (code serial PRIMARY KEY,  categoryText VARCHAR(200), displayLevel NUMERIC(2), selectable VARCHAR(1)  );
+CREATE TABLE IF NOT EXISTS ors.category (code serial PRIMARY KEY,  categoryText VARCHAR(200), displayLevel NUMERIC(2), selectable VARCHAR(1)  );
